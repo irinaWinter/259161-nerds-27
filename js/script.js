969bfdb33f09116a0d807e948ce03b5667cd9312
Itina Winter <lobanova.irina.96@mail.ru>
2020-02-16 10:41:06 +0300

    Объединяет скрипты в один файл

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// slider
+
+var dot = document.querySelectorAll('.dot');
+var slide = document.querySelectorAll('.slide');
+var current = 0;
+
+dot.forEach(function(e, i) {
+  e.addEventListener('click', function() {
+    if (i !== current) {
+    	slide[current].classList.add('hidden');
+    	slide[i].classList.remove('hidden');
+    	dot[current].classList.remove('dot-active');
+    	dot[i].classList.add('dot-active');
+			current = i;
+    }
+  }); 	
+});
+
+
+// popup
+
 var body = document.querySelector('body');
 var container_popup = document.querySelector('.container-popup');
 var popup = document.querySelector('.container-popup');
